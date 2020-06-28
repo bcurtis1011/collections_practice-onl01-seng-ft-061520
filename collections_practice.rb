95a120b8e7bfb78f1eee227b40a5fa09511efb92
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 19:29:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-  index == 1 ? element : element[element.length] = "s"
+    index == 1 ? element : element[element.length] = "s"
   end
   array
 end
