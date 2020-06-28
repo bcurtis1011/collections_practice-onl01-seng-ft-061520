446c308fd2859f8bab794cb1e1b05c93b33a94b9
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 18:45:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,10 +2,10 @@ def sort_array_asc(array_asc)
   array.sort
 end
 
-def sort_array_desc(array)
+def sort_array_desc(array_desc)
   array.sort{|a, b| b <=> a}
 end
 
-def sort_array_char_count(array)
+def sort_array_char_count(array_char)
   
 end
